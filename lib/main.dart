c16679a97c5cdbc0a68b98b96c0a9861619d0c96
saozcan <samimozcan@outlook.com>
2022-12-20 20:42:10 +0300

    Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,60 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-      debugShowCheckedModeBanner: false,
-    ),
-  );
+// void main() {
+//   runApp(
+//     GetMaterialApp(
+//       title: "Application",
+//       initialRoute: AppPages.INITIAL,
+//       getPages: AppPages.routes,
+//       debugShowCheckedModeBanner: false,
+//     ),
+//   );
+// }
+
+
+//Timer
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String greeting = "Hello";
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), (){
+      setState(() {
+        greeting = "After Some Time";
+      });
+    });
+  }
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      title: "Material App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Material App Bar"),
+        ),
+        body: Center(child: Column(
+          children: [
+            Text(greeting, style: TextStyle(fontSize: 30),)
+          ],
+        ),),
+      ),
+    );
+  }
+  
 }
